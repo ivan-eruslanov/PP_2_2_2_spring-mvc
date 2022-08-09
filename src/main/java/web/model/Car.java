1a4fc448a41c1b7096774ffbc914111f09712239
eruslanov-ivan <iv.eruslanov@yandex.ru>
2022-08-09 20:06:26 +0500

    first commit

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,40 @@
+package web.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+    private String model;
+    private int series;
+    private String cost;
+
+    public Car() {
+    }
+
+    public Car(String model, int series, String cost) {
+        this.model = model;
+        this.series = series;
+        this.cost = cost;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                ", cost='" + cost + '\'' +
+                '}';
+    }
+}
